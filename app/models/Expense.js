75232fa2202ead2b790f3f041e91fa1f2f237b19
kipngeno <kipngeno2sendyit.com>
2021-03-01 19:26:33 +0300

    added expense model

---
+++ app/models/Expense.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const ExpenseSchema = new mongoose.Schema({
+  title:{
+    type: String,
+    required: true
+  },
+  amount:{
+    type: Number,
+    required: true
+  },
+  status:{
+    type:Boolean,
+    default: true
+  }  
+},
+{
+  timestamps:true
+}
+)
+
+module.exports = mongoose.model('Expense', ExpenseSchema)
