c768e5468e9b315021ccb02ca61bae9b471564c0
kipngeno-isaac <kipngeno.isaac@students.kisiiuniversity.ac.ke>
2023-05-20 22:24:16 +0300

    Add openapi specs

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,32 +2,6 @@ const express = require("express");
 const router = express.Router();
 const ExpenseController = require("../controllers/ExpenseController");
 
-
-/**
- * @swagger
- * /expenses:
- *   post:
- *     summary: Create an Expense.
- *     responses:
- *       201:
- *         description: Created
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: object
- *                   properties:
- *                     title:
- *                       type: string
- *                       description: The name of the expense.
- *                       example: shopping
- *                     amount:
- *                       type: number
- *                       description: The cost of the expense.
- *                       example: 500
-*/
 router.post("/expenses", ExpenseController.create);
 router.get("/expenses", ExpenseController.findAll);
 router.get("/expenses/:id", ExpenseController.findOne);
