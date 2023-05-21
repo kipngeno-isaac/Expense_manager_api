ecae6fc2de4b65a3065b411c63d6b48503908d6c
kipngeno-isaac <kipngeno.isaac@students.kisiiuniversity.ac.ke>
2023-05-21 19:20:34 +0300

    remove swagger config

--- index.js
+++ index.js
@@ -2,25 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-
-const swaggerDefinition = {
-  openapi: '3.0.0',
-  info: {
-    title: 'Express API for JSONPlaceholder',
-    version: '1.0.0',
-  },
-};
-
-const options = {
-  swaggerDefinition,
-  // Paths to files containing OpenAPI definitions
-  apis: ['./app/routes/*.js'],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
-
 
 dotenv.config();
 
@@ -34,7 +15,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const routes = require('./app/routes');
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/api', routes);
 
